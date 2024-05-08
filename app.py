094ffd8168b644899c5dd481491e26b02cd00236
Rahul verma <100432854+NME-rahul@users.noreply.github.com>
2024-05-08 19:11:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,9 @@ import threading
 
 app = Flask(__name__)
 
-
-app.config['Tempearture'] = ""
-app.config['dataFile'] = ""
-app.config['image'] = ""
-
-def process_file():
+'''
+def process_file(image,):
     pass
-
 
 @app.route('/files/')
 def index():
@@ -20,13 +15,14 @@ def index():
         dataFile = request.files['dataFile']
     return render_template('/static/files/settings.html')
 
+'''
+
 
 @app.route('/')
 def index():
     if 'fileInput' in request.files:
         image = request.files['fileInput']
         threading.Thread(target=process_file, args=(image,)).start()
-        
     return render_template('index.html')
 
         
