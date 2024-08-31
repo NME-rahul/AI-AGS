b7c91636323419ccd1c5df05a070c40ab9c8e263
Rahul verma <100432854+NME-rahul@users.noreply.github.com>
2024-08-31 18:38:03 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -4,20 +4,3 @@ Created on Sat Jun  1 00:18:05 2024
 
 @author: rahul
 """
-
-import tkinter as tk
-
-class GUI(tk.Tk):
-  def __init__(self):
-    super.().__init__()
-        
-  def mainPage(self):
-    pass
-        
-  def Settings(self):
-    pass
- 
-
-if __name__ == "__main__":
-  app = GUI()
-  app.mainloop()
